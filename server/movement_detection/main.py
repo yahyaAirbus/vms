9cc35499d794673383007de1a05751a01fc2ad22
yahyaAirbus <yahya.khafif@airbus.com>
2024-08-08 11:15:27 +0300

    model working with recorded video

--- server/movement_detection/main.py
+++ server/movement_detection/main.py
@@ -156,7 +156,7 @@ CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus"
 
 # Parameters for detection
 args = {
-    "confidence": 0.7,
+    "confidence": 0.65,
     "skip_frames": 10,
     "view": False,
     "threshold": 4000
@@ -170,105 +170,106 @@ found, avg = False, None
 outVideo = None
 
 # Fetch the live stream URLs
-stream_urls = "http://127.0.0.1:8083/stream/demoStream/channel/1/hls/live/index.m3u8"
-if not stream_urls:
-    print("[ERROR] No stream URLs found")
-else:
-    def process_frame():
-        for url in stream_urls:
-            print(f"[INFO] Processing stream: {url}")
-            vs = getFrame(url, None)
+test_video_path = "/Users/yahya/Desktop/prog/vms/server/movement_detection/test_video.mov"
+
+# Main processing function
+def process_frame(video_path):
+    global totalFrames, newImage, smallFrame, image_sent, found, avg, outVideo
+
+    print(f"[INFO] Processing video: {video_path}")
+    vs = getFrame(video_path, None)
     
-            while True:
-                notToSkip = totalFrames % args["skip_frames"] == 0
-                if cv2.waitKey(10) & 0xFF == ord('q'):
-                    print("[INFO] Quit signal received, exiting...")
-                    break
-                rt, frame = vs.read()
-                if not rt:
-                    print("[WARN] Frame read unsuccessful, restarting stream...")
-                    totalFrames = 0
-                    if outVideo:
-                        outVideo.release()
-                        outVideo = None
-                    vs = getFrame(url, None)
+    bearer_token = auth(AGNET_USERNAME, AGNET_PASSWORD, AGNET_CLIENT_ID)
+    
+    while True:
+        notToSkip = totalFrames % args["skip_frames"] == 0
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            print("[INFO] Quit signal received, exiting...")
+            break
+        rt, frame = vs.read()
+        if not rt:
+            print("[WARN] Frame read unsuccessful, restarting stream...")
+            totalFrames = 0
+            if outVideo:
+                outVideo.release()
+                outVideo = None
+            vs = getFrame(video_path, None)
+            continue
+        if notToSkip:
+            smallFrame = imutils.resize(frame, width=500)
+            grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            grey = cv2.GaussianBlur(grey, (21, 21), 0)
+
+            if avg is None or totalFrames > 600:
+                totalFrames = 0
+                print("[INFO] Starting background model...")
+                avg = grey.copy().astype("float")
+                continue
+
+            cv2.accumulateWeighted(grey, avg, 0.5)
+            frameDelta = cv2.absdiff(grey, cv2.convertScaleAbs(avg))
+
+            thresh = cv2.threshold(frameDelta, 5, 255, cv2.THRESH_BINARY)[1]
+            thresh = cv2.dilate(thresh, None, iterations=2)
+            (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            motion = False
+            for c in cnts:
+                if cv2.contourArea(c) < args['threshold']:
                     continue
-                if notToSkip:
-                    smallFrame = imutils.resize(frame, width=500)
-                    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                    grey = cv2.GaussianBlur(grey, (21, 21), 0)
-    
-                    if avg is None or totalFrames > 600:
-                        totalFrames = 0
-                        print("[INFO] Starting background model...")
-                        avg = grey.copy().astype("float")
-                        continue
-    
-                    cv2.accumulateWeighted(grey, avg, 0.5)
-                    frameDelta = cv2.absdiff(grey, cv2.convertScaleAbs(avg))
-    
-                    thresh = cv2.threshold(frameDelta, 5, 255, cv2.THRESH_BINARY)[1]
-                    thresh = cv2.dilate(thresh, None, iterations=2)
-                    (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
-                    motion = False
-                    for c in cnts:
-                        if cv2.contourArea(c) < args['threshold']:
-                            continue
-    
-                        (x, y, w, h) = cv2.boundingRect(c)
-                        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                        motion = True
-                        break
-    
-                    if motion:
-                        print("[INFO] Motion detected...")
-                        totalFrames = 0
-                        rgb = cv2.cvtColor(smallFrame, cv2.COLOR_BGR2RGB)
-                        if W is None or H is None:
-                            (H, W) = smallFrame.shape[:2]
-                        found, newImage = getDetection(args, smallFrame, net, CLASSES)
-    
-                if found:
-                    cur = datetime.now()
-                    checkcur = cur - timedelta(minutes=1)
-                    if checkcur > image_sent:
-                        image_sent = cur
-                        # Save the frame as an image
-                        image_path = "detected_frame.jpg"
-                        cv2.imwrite(image_path, frame)
-                        send_emergency_message(auth(AGNET_USERNAME, AGNET_PASSWORD, AGNET_CLIENT_ID),"358408346118")
-                        send_message(auth(AGNET_USERNAME, AGNET_PASSWORD, AGNET_CLIENT_ID), "358408346118", image_path)
-                        width = int(vs.get(cv2.CAP_PROP_FRAME_WIDTH))
-                        height = int(vs.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-                        timestr = time.strftime("%Y%m%d-%H%M%S.mp4")
-                        timestr = "HumanDetectionRecord/" + timestr
-                        if not outVideo:
-                            outVideo = cv2.VideoWriter(timestr, fourcc, 15.0, (width, height))
-                            print("[INFO] Started recording video:", timestr)
-    
-                if args["view"]:
-                    cv2.imshow('frame', frame)
-                    if newImage is not None:
-                        cv2.imshow('newImage', newImage)
-                    if grey is not None:
-                        cv2.imshow('grey', grey)
-    
-                if outVideo:
-                    outVideo.write(frame)
-                    if not found and not motion:
-                        print("[INFO] Stopping video recording")
-                        outVideo.release()
-                        outVideo = None
-                totalFrames += 1
 
-                yield frame, newImage
+                (x, y, w, h) = cv2.boundingRect(c)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                motion = True
+                break
 
-            if vs is not None:
-                vs.release()
-            cv2.destroyAllWindows()
-        print("[INFO] Finished processing all streams")
+            if motion:
+                W, H = None, None
+                print("[INFO] Motion detected...")
+                totalFrames = 0
+                rgb = cv2.cvtColor(smallFrame, cv2.COLOR_BGR2RGB)
+                if W is None or H is None:
+                    (H, W) = smallFrame.shape[:2]
+                found, newImage = getDetection(args, smallFrame, net, CLASSES)
+
+        if found:
+            cur = datetime.now()
+            checkcur = cur - timedelta(minutes=1)
+            if checkcur > image_sent:
+                image_sent = cur
+                # Save the frame as an image
+                image_path = "detected_frame.jpg"
+                cv2.imwrite(image_path, frame)
+                send_emergency_message(bearer_token, "358408346118")
+                send_message(bearer_token, "358408346118", image_path)
+                width = int(vs.get(cv2.CAP_PROP_FRAME_WIDTH))
+                height = int(vs.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+                timestr = time.strftime("%Y%m%d-%H%M%S.mp4")
+                timestr = "HumanDetectionRecord/" + timestr
+                if not outVideo:
+                    outVideo = cv2.VideoWriter(timestr, fourcc, 15.0, (width, height))
+                    print("[INFO] Started recording video:", timestr)
+
+        if args["view"]:
+            cv2.imshow('frame', frame)
+            if newImage is not None:
+                cv2.imshow('newImage', newImage)
+            if grey is not None:
+                cv2.imshow('grey', grey)
+
+        if outVideo:
+            outVideo.write(frame)
+            if not found and not motion:
+                print("[INFO] Stopping video recording")
+                outVideo.release()
+                outVideo = None
+        totalFrames += 1
+
+    if vs is not None:
+        vs.release()
+    cv2.destroyAllWindows()
+    print("[INFO] Finished processing video")
 
 if __name__ == "__main__":
-   process_frame()
+    process_frame(test_video_path)
